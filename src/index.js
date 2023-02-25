73c281473ba1dcabc6860bf1e0f3e6ac696c35d6
bennikabs <bennykabs90@gmail.com>
2023-02-25 16:18:06 +0100

    remodelled the page

--- src/index.js
+++ src/index.js
@@ -43,6 +43,55 @@ console.log(now.getMinutes());
 let currentDate = document.querySelector("#week-day");
 currentDate.innerHTML = `${day} ${nowdate} ${month}  ${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+         <img src="http://openweathermap.org/img/wn/${
+           forecastDay.weather[0].icon
+         }@2x.png" 
+            id="icon"
+            alt="" 
+            width="45"/>
+        <div class="weather-forecast-temperatures">
+          <span class="col-3"> ${Math.round(forecastDay.temp.max)}° </span>
+          <span class="col-3"> ${Math.round(forecastDay.temp.min)}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "c68d952d49c1f7b73f7353b16aa88d42";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayFahrenheiTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#tem-perat");
@@ -85,6 +134,8 @@ function showWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 let celsiusTemperature = null;
